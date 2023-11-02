ebf5517d863b467384e06fc2774cd63a67aca40d
geumyoung <1gyn093@gmail.com>
2023-11-02 22:20:12 +0900

    isOpen 상태값에 따라 cart 노출 결정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+
 function App() {
+
+  const isOpenCart = useSelector((state) => state.cart.isOpen);
+
   return (
     <Layout>
-      <Cart />
+      {isOpenCart? <Cart /> : null}
       <Products />
     </Layout>
   );
