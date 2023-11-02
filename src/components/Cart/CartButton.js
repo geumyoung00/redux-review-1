1a4ce61656a4222017990cea69ef95fefa8da1f7
geumyoung <1gyn093@gmail.com>
2023-11-02 22:27:12 +0900

    카트 open을 위한 동작 연결하기

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cartSlice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const oepnCartHandler = () => { 
+    dispatch(cartActions.isOpen());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={oepnCartHandler} >
       <span>🛒</span>
       <span className={classes.badge}>1</span>
     </button>
