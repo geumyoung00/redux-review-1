b3f9fdc7e272263a85cbc00d01933791303da32b
geumyoung <1gyn093@gmail.com>
2023-11-07 22:00:40 +0900

    manageCartSlice에서 current 삭제, cartButton badge 완료

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { openCartActions } from '../../store/openCartSlice';
 import classes from './CartButton.module.css';
 
@@ -8,10 +8,19 @@ const CartButton = (props) => {
     dispatch(openCartActions.open());
   }
   
-  return (
+  // 1. product item이 담긴 배열 items를 가져온다.
+  const cartItems = useSelector((state) => state.manageCart.items);
+
+  // 2. items 객체의 quantity 값만 합산한다. (reduce로)
+  const totalQuantity = cartItems.reduce((prev, current) => {
+    return prev + current.quantity
+  }, 0);
+
+return (
     <button className={classes.button} onClick={openCartHandler} >
       <span>🛒</span>
-      <span className={classes.badge}>1</span>
+      {/* 3. badge에 연결해준다. */}
+    <span className={classes.badge}>{ totalQuantity }</span>
     </button>
   );
 };
