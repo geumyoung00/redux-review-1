b5372b8e38ae8be04ed60f4bd8702f6bfbde1960
geumyoung <1gyn093@gmail.com>
2023-11-14 01:52:19 +0900

    redux toolkit Thunk updateCart 생성 후 dispatch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { uiActions } from './store/uiSlice';
+// import { uiActions } from './store/uiSlice';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification/Notification';
+import { updateCart } from './store/cart-actions';
 
 function App() {
   const isOpenCart = useSelector((state) => state.ui.isOpen);
@@ -16,44 +17,7 @@ function App() {
 
   // 상태값이 업데이트 될 때만 실행된다.
   useEffect(() => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'pending',
-        message: 'pending...',
-      }),
-    );
-
-    const applyApi = async () => {
-      const res = await fetch(
-        'https://cart-data-cc615-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cartList),
-        },
-      );
-
-      if (!res.ok) {
-        throw new Error('api 전송 중 실패...');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'success',
-          message: 'success...',
-        }),
-      );
-    };
-    applyApi().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error',
-          message: 'PUT error...',
-        }),
-      );
-    });
+    dispatch(updateCart(cartList));
   }, [dispatch, cartList]);
 
   return (
--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,8 +1,58 @@
+import { uiActions } from './uiSlice';
+
 // Thunk : 함수, 액션 생성자. 액션을 리턴해주는 함수를 리턴하는 함수.
 // Thunk의 리턴되는 함수의 인자로 redux toolkit이 자동으로 dispatch를 부여.
 
 // PUT: update DB
-export const updateCart = () => {};
+export const updateCart = (cartList) => {
+  return async (dispatch) => {
+    // fetch 전 상태값을 pending으로
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Penging',
+        message: 'Pending...',
+      }),
+    );
+
+    // PUT 메소드로 fetch
+    const sendRequest = async () => {
+      const res = await fetch(
+        'https://cart-data-cc615-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cartList),
+        },
+      );
+
+      if (!res.ok) {
+        throw new Error('api 전송 중 실패...');
+      }
+    };
+
+    // try, catch로 fetch 실행
+    try {
+      // 전송 성공시 상태값.
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success',
+          message: 'Success!',
+        }),
+      );
+    } catch (error) {
+      // error 발생시 상태값
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'PUT error...',
+        }),
+      );
+    }
+  };
+};
 
 // GET : fetch DB data
 export const fetchCart = () => {};
